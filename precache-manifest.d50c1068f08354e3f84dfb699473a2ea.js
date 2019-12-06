c564a53865490bc74ac08d82ebabe9b88b76f951
Max Verba <maxwillow93@gmail.com>
2019-12-07 00:03:16 +0200

    Updates

---
+++ precache-manifest.d50c1068f08354e3f84dfb699473a2ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323bf4e32a3550ed619a45baefabef42",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "bd01a4dcf35a9f27bac2",
+    "url": "/goit-react-hw-01-components/static/css/main.ebf59337.chunk.css"
+  },
+  {
+    "revision": "70e74add56af855f0608",
+    "url": "/goit-react-hw-01-components/static/js/2.26ef0df9.chunk.js"
+  },
+  {
+    "revision": "bd01a4dcf35a9f27bac2",
+    "url": "/goit-react-hw-01-components/static/js/main.99953464.chunk.js"
+  },
+  {
+    "revision": "42eca5c9d0b6ddf0c746",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.094ae600.js"
+  }
+]);
